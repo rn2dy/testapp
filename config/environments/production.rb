cd7925c92c4acf835ef581989b2fa4ca0aa4f999
baboonworksfine <baboonworksfine@baboonworksfine.(none)>
2012-03-16 18:25:21 -0500

    try different smtp configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,19 @@ Testapp::Application.configure do
   # ActionMailer Config  
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  # Setup for production - deliveries, no errors raised
+  config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
+  
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => "plain",
+    :user_name => "smartplaymonks@gmail.com",
+    :password => "smartmonks",
+    :enable_starttls_auto => true
+  }
 
 
 end
