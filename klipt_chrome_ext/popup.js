09eea876b03270efb2cfe1682219302719460afe
baboonworksfine <baboonworksfine@baboonworksfine.(none)>
2012-04-28 16:27:14 -0500

    make bookmarklet accessible with/without authenticed users

---
+++ klipt_chrome_ext/popup.js
@@ -0,0 +1,30 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var req = new XMLHttpRequest();
+req.open(
+    "GET",
+    "http://www.klipt.me/bookmarklet/new_link?url=" + encodeURIComponent(window.location.href) + "&title=" + encodeURIComponent(document.title),
+    true);
+req.onload = showPhotos;
+req.send(null);
+
+function showPhotos() {
+  var photos = req.responseXML.getElementsByTagName("photo");
+
+  for (var i = 0, photo; photo = photos[i]; i++) {
+    var img = document.createElement("image");
+    img.src = constructImageURL(photo);
+    document.body.appendChild(img);
+  }
+}
+
+// See: http://www.flickr.com/services/api/misc.urls.html
+function constructImageURL(photo) {
+  return "http://farm" + photo.getAttribute("farm") +
+      ".static.flickr.com/" + photo.getAttribute("server") +
+      "/" + photo.getAttribute("id") +
+      "_" + photo.getAttribute("secret") +
+      "_s.jpg";
+}
